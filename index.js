10046c34cd193aba9a7706e98772d7c85f062a42
hazimfarhan17 <b022210107@student.utem.edu.my>
2023-10-31 12:03:56 +0800

    Fifth Commit

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.post('/kirara', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-///app.post('/api/echo', (req, res) => {
-
-///})
+app.post('/login', (req, res) => {
+    if (req.body.username !== 'Apip') {
+        res.status(400).send('Invalid username')
+        return
+    }
+    if (req.body.password !== 'Apip123') {
+        res.status(401).send('Invalid password')
+        return
+    }
+    res.send('login successfully')
+})
 
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
