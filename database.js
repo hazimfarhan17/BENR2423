259bb1cc762202a1ca560d4d79eea08215aa7a4a
hazimfarhan17 <b022210107@student.utem.edu.my>
2023-11-02 21:35:25 +0800

    [7] commit

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://b022210107:<987654321>@cluster0.hbjf00x.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Hazim:987654321@cluster0.hbjf00x.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
